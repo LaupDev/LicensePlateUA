04d7e4695dc635c81d64c0f6fc4b3b86df6568a8
Pavlo Penzelyk <p.penzelik@gmail.com>
2022-12-28 01:21:47 +0200

    Initial commit

---
+++ app/src/main/java/com/laupdev/licenseplateua/presentation/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.laupdev.licenseplateua.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.laupdev.licenseplateua.presentation.ui.theme.LicensePlateUATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
