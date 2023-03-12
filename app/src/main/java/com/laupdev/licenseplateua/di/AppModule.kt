e26bcfe6a8a73a2b61ea72d9c67dc3f2c7f3c12f
Pavlo Penzelyk <p.penzelik@gmail.com>
2023-03-13 01:35:05 +0200

    Added headers to HTTP client

--- app/src/main/java/com/laupdev/licenseplateua/di/AppModule.kt
+++ app/src/main/java/com/laupdev/licenseplateua/di/AppModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
@@ -12,14 +13,26 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
+object AppModule {
 
     @Provides
     @Singleton
     fun provideLicensePlateApi(): LicensePlateApi {
+        val client = OkHttpClient.Builder().addInterceptor { chain ->
+            val request = chain.request().let { original ->
+                original.newBuilder()
+                    .header("X-Api-Key", LicensePlateApi.API_KEY)
+                    .header("Accept", "application/json\\r\\n")
+                    .method(original.method(), original.body())
+                    .build()
+            }
+            chain.proceed(request)
+        }.build()
+
         return Retrofit.Builder()
             .baseUrl(LicensePlateApi.BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
             .build()
             .create()
     }
