0249d7730e1e4d670cc828e9c07b85cb6cd778c1
LaupDev <p.penzelik@gmail.com>
2023-07-27 23:54:49 +0300

    Minor UI update

--- app/src/main/java/com/laupdev/licenseplateua/presentation/MainActivity.kt
+++ app/src/main/java/com/laupdev/licenseplateua/presentation/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.laupdev.licenseplateua.presentation.search_history.NavGraphs
 import com.laupdev.licenseplateua.presentation.ui.theme.LicensePlateUATheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            LicensePlateUATheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DestinationsNavHost(navGraph = NavGraphs.root)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/laupdev/licenseplateua/presentation/search_history/LicensePlatesSearchHistoryScreen.kt
+++ app/src/main/java/com/laupdev/licenseplateua/presentation/search_history/LicensePlatesSearchHistoryScreen.kt
@@ -1,20 +1,32 @@
 package com.laupdev.licenseplateua.presentation.search_history
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 @Destination(start = true)
 fun LicensePlatesSearchHistoryScreen(
@@ -22,25 +34,44 @@ fun LicensePlatesSearchHistoryScreen(
     viewModel: LicensePlatesSearchHistoryViewModel = hiltViewModel()
 ) {
     var text by remember { mutableStateOf("") }
-    var active by remember { mutableStateOf(false) }
 
-    Column(
+    Scaffold(
         modifier = Modifier.fillMaxSize()
     ) {
-        SearchBar(
-            query = text,
-            onQueryChange = {
-                text = it
-            },
-            onSearch = {
-                     active = false
-            },
-            active = active,
-            onActiveChange = {
-                active = it
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .consumeWindowInsets(it)
+                .padding(it),
         ) {
+            SearchBar(
+                query = text,
+                onQueryChange = {
+                    text = it
+                },
+                onSearch = {
+                },
+                active = false,
+                onActiveChange = {
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                placeholder = {
+                    Text(text = "XX0000XX")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+                },
+                trailingIcon = {
+                    Icon(imageVector = Icons.Default.Close, contentDescription = "Clear")
+                }
+            ) {
 
+            }
+            LazyColumn() {
+
+            }
         }
     }
 }
