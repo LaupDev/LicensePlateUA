2d0d86b7c4e86cbad9c76a0a2ae9e10c906c73b9
Pavlo Penzelyk <p.penzelik@gmail.com>
2023-08-26 01:10:09 +0300

    Provided Coroutine Dispatchers for DI and updated UseCases accordingly

--- app/src/main/java/com/laupdev/licenseplateua/domain/usecases/GetLicensePlateInfoUseCaseImpl.kt
+++ app/src/main/java/com/laupdev/licenseplateua/domain/usecases/GetLicensePlateInfoUseCaseImpl.kt
@@ -1,17 +1,17 @@
 package com.laupdev.licenseplateua.domain.usecases
 
+import com.laupdev.licenseplateua.di.IoDispatcher
 import com.laupdev.licenseplateua.domain.model.LicensePlateInfo
 import com.laupdev.licenseplateua.domain.repository.LicensePlatesRepository
 import com.laupdev.licenseplateua.util.Resource
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetLicensePlateInfoUseCaseImpl @Inject constructor(
     private val licensePlatesRepository: LicensePlatesRepository,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) {
 
     operator fun invoke(licensePlate: String): Flow<Resource<LicensePlateInfo>> {
--- app/src/main/java/com/laupdev/licenseplateua/domain/usecases/GetLicensePlatesSearchHistoryUseCaseImpl.kt
+++ app/src/main/java/com/laupdev/licenseplateua/domain/usecases/GetLicensePlatesSearchHistoryUseCaseImpl.kt
@@ -1,18 +1,19 @@
 package com.laupdev.licenseplateua.domain.usecases
 
+import com.laupdev.licenseplateua.di.IoDispatcher
 import com.laupdev.licenseplateua.domain.model.LicensePlateMainInfo
 import com.laupdev.licenseplateua.domain.repository.LicensePlatesRepository
 import com.laupdev.licenseplateua.util.Resource
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetLicensePlatesSearchHistoryUseCaseImpl @Inject constructor(
-    private val licensePlatesRepository: LicensePlatesRepository
+    private val licensePlatesRepository: LicensePlatesRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) {
     operator fun invoke(): Flow<Resource<List<LicensePlateMainInfo>>> {
-        return licensePlatesRepository.getLicensePlatesSearchHistory()
+        return licensePlatesRepository.getLicensePlatesSearchHistory().flowOn(dispatcher)
     }
 }
